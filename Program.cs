7ed1e2fc1f5a1a6ee0473a0ba91bdaa58621a490
Martin Matasovic <person@martinlastname.com>
2022-04-04 23:06:48 -0700

    Raylib hello world.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Raylib_cs;
+
+namespace HelloWorld 
+{
+	static class Program
+	{	
+		public static void Main()
+		{	
+			Raylib.InitWindow(800, 480, "Hello World");
+			Raylib.SetTargetFPS(120);
+
+			while (!Raylib.WindowShouldClose())
+			{	
+				Raylib.BeginDrawing();
+				Raylib.ClearBackground(Color.WHITE);
+
+				Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+
+				Raylib.EndDrawing(); 
+			}
+
+			Raylib.CloseWindow();
+		}
+	}
+}
